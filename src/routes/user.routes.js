bab71c6b0e3137c7e3fcefbf3ef6b820b00dba17
avserver-16 <avish.vijay2021@gmail.com>
2026-05-15 15:42:38 +0530

    feat: updates that were required for user onboarding, login,ogout wa upadated

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,13 +6,19 @@ const {
   getUsers,
   getUserById,
   updateUser,
+  checkUserByPhone,
   deleteUser,
+  loginUser,
+  logoutUser,
 } = require("../controllers/user.controller");
 
 router.post("/", createUser);
 router.get("/", getUsers);
 router.get("/:id", getUserById);
 router.put("/:id", updateUser);
+router.post("/check-phone", checkUserByPhone);
+router.post("/login", loginUser);
 router.delete("/:id", deleteUser);
+router.post("/logout", logoutUser);
 
 module.exports = router;
