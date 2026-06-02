a8774d9f7763211bbe86f8403c313703b7d5024e
avserver-16 <avish.vijay2021@gmail.com>
2026-06-02 23:19:08 +0530

    feat: added roms and quadilaterals to that roojm

--- src/routes/spatial.routes.js
+++ src/routes/spatial.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
 const router = express.Router();
 
 const {
-    getNearbyBuildings
+    getNearbyBuildings,
+    getNearbyRooms,
 } = require("../controllers/spatial.controller");
 
 router.post("/nearby", getNearbyBuildings);
+router.post("/nearby-rooms", getNearbyRooms);
 
 module.exports = router;
